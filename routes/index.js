9ccd83d753acda58be794d6e53c7dea710abe47f
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-02-27 15:42:04 -0700

    Updated Work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+let api = require("./api");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use("/api", api);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
