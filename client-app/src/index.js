bd20d4546baf488124a11cb7def491fe41107f16
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-03-02 12:04:34 -0700

    Create SIgn In

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 import store from "./store";
