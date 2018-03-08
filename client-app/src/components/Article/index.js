ec1c36c89d19f7820e85b6bd1e0ae9d5ec8a02a2
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-03-08 14:41:55 -0700

    Comments and Comment end point

--- client-app/src/components/Article/index.js
+++ client-app/src/components/Article/index.js
@@ -5,6 +5,8 @@ import marked from "marked";
 
 import services from "../../services";
 
+import CommentContainer from "./CommentContainer";
+
 const mapStateToProps = state => ({
     ...state.article,
     currentUser: state.common.currentUser
@@ -21,7 +23,9 @@ class Article extends Component {
         and the comments for this. 2 promises. ummmmm
         */
         const articleId = this.props.match.params.id;
-        this.props.onLoad(Promise.all([services.Articles.get(articleId)]));
+        this.props.onLoad(
+            Promise.all([services.Articles.get(articleId), services.Comments.forArticle(articleId)])
+        );
     }
 
     /*
@@ -33,7 +37,7 @@ class Article extends Component {
             return null;
         }
         const markup = { __html: marked(article.body) };
-        const canModify = this.props.currentUser.username === article.author.username;
+        // const canModify = this.props.currentUser.username === article.author.username;
         return (
             <div className="article-page">
                 <div className="banner">
@@ -62,9 +66,15 @@ class Article extends Component {
                     <hr />
 
                     <div className="article-actions" />
-
-                    <div className="row" />
                 </div>
+
+                <div className="row" />
+                <CommentContainer
+                    comments={this.props.comments || []}
+                    errors={this.props.commentErrors}
+                    slug={this.props.match.params.id}
+                    currentUser={this.props.currentUser}
+                />
             </div>
         );
     }
